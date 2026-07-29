1374a31bd6577d6c9344416e9fa4bc36fe1b1d67
RicHo542 <richard.kg.horn@gmail.com>
2026-07-29 19:38:13 +0200

    start command template

---
+++ internal/cli/start.go
@@ -0,0 +1,43 @@
+package cli
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func newStartCmd(app *App) *cobra.Command {
+	var (
+		createBranch bool
+		message      string
+	)
+
+	cmd := &cobra.Command{
+		Use:   "start",
+		Short: "Assign and start work item",
+		Long: `Assigns and sets a work item to 'in progress'.
+
+Use '-b' to also create a new feature branch to directly create.
+Use '-m' to comment on the work item.`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+
+			if app.Context == nil {
+				return fmt.Errorf("not initialized: run 'sneak init' first")
+			}
+
+			return nil
+		},
+	}
+
+	cmd.Flags().BoolVarP(&createBranch, "branch", "b", false, "create a feature branch in current git.")
+	cmd.Flags().StringVarP(&message, "message", "m", "", "comment to add to the work item(s) to be started")
+
+	return cmd
+}
+
+func runStartCommand(
+	app *App, tasks []string,
+	createBranch bool, comment string,
+) error {
+	return nil
+}
