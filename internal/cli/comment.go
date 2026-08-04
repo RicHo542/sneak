b6a32c38e5cb1bbe7164d1b23faf5cf0910efdf6
RicHo542 <richard.kg.horn@gmail.com>
2026-08-04 17:05:59 +0200

    feat: comment command

---
+++ internal/cli/comment.go
@@ -0,0 +1,69 @@
+package cli
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/richo542/sneak/internal/ui"
+	"github.com/spf13/cobra"
+)
+
+func newCommentCmd(app *App) *cobra.Command {
+	var (
+		comment string
+	)
+
+	cmd := &cobra.Command{
+		Use:   "comment",
+		Short: "Comment on a given task",
+		Long:  `Allows you to leave a comment on a work item.`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+
+			if app.Context == nil {
+				return fmt.Errorf("not initialized: run 'sneak init' first")
+			}
+
+			return runComment(app, args, comment)
+		},
+	}
+
+	cmd.Flags().StringVarP(&comment, "message", "m", "", "message to add as a comment")
+	cmd.MarkFlagRequired("message")
+
+	return cmd
+}
+
+func runComment(app *App, tasks []string, comment string) error {
+
+	refreshRequired, err := CheckAndRefreshCache(app, false)
+	if refreshRequired && err != nil {
+		return err
+	}
+
+	// Validate tasks names
+	invalidTasks, invalid := HasInvalidTasks(app, tasks)
+	if invalid {
+		for _, ivTask := range invalidTasks {
+			ui.Printfln("task '%s' cannot be found.", ivTask)
+		}
+		return fmt.Errorf("Consider running 'sneak list --refresh' to update.")
+	}
+
+	// Error can be ignored as it would raise in case an item
+	// cannot be found - This is checked beforehand.
+	cachedTasks, _ := app.State.Cache.GetByKeyBatch(tasks)
+
+	comment = strings.TrimSpace(comment)
+	if comment == "" {
+		return fmt.Errorf("please provide a valid comment using '-m'.")
+	}
+
+	if err := app.Client.AddCommentToWorkItems(
+		app.Context, cachedTasks, comment,
+	); err != nil {
+		ui.Printfln("Error: %w", err)
+		return err
+	}
+
+	return nil
+}
