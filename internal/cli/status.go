b1ca6efa82afb5480e0d201b3181ebeb2e835d91
RicHo542 <richard.kg.horn@gmail.com>
2026-08-09 15:44:09 +0200

    feat: simple status command

---
+++ internal/cli/status.go
@@ -0,0 +1,29 @@
+package cli
+
+import (
+	"github.com/richo542/sneak/internal/ui"
+	"github.com/spf13/cobra"
+)
+
+func newStatusCmd(app *App) *cobra.Command {
+
+	cmd := &cobra.Command{
+		Use:   "status",
+		Short: "Lists the currently active tasks",
+		Long: `List all tasks that are currently set to active and are managed by sneak.
+
+Useful to get a quick glance at what to close and what was started last time you picked up your codebase.`,
+		Args: cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return runStatusCmd(app)
+		},
+	}
+
+	return cmd
+}
+
+func runStatusCmd(app *App) error {
+	ui.PrintActiveTaskTable(app.State.ActiveTasks)
+	ui.Printfln("%d active items", len(app.State.ActiveTasks))
+	return nil
+}
