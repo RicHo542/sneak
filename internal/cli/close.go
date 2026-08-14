a73efd30b0ccc4854f125a8828d0b9e5a8db510d
RicHo542 <richard.kg.horn@gmail.com>
2026-08-14 11:51:44 +0200

    chore: handlers

--- internal/cli/close.go
+++ internal/cli/close.go
@@ -2,10 +2,8 @@ package cli
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/richo542/sneak/internal/config"
-	"github.com/richo542/sneak/internal/ui"
 	"github.com/spf13/cobra"
 )
 
@@ -49,7 +47,7 @@ func runCloseCmd(
 	}
 
 	// resolve and forward
-	cacheItems, err := resolveTaskFocus(app, taskKeys, all)
+	cacheItems, err := ResolveTaskFocus(app, taskKeys, all)
 	if err != nil {
 		return err
 	}
@@ -57,49 +55,13 @@ func runCloseCmd(
 	return processCloseCmd(app, cacheItems, comment)
 }
 
-func resolveTaskFocus(app *App, taskKeys []string, all bool) ([]*config.CacheItem, error) {
-	if all {
-		cacheItems, err := app.State.GetActiveCacheItems()
-		if err != nil {
-			return nil, err
-		}
-		return cacheItems, nil
-	}
-
-	selectedKeys := taskKeys
-	tuiSelectErr := fmt.Errorf("failed task selection.")
-	// Prompt user with interactive selection if no key was provided.
-	if len(taskKeys) < 1 {
-		selectedKeys, tuiSelectErr = ui.InteractiveSelectItem(app.State.Cache.Items)
-		if tuiSelectErr != nil {
-			return nil, tuiSelectErr
-		}
-	}
-
-	cacheItems, err := app.State.Cache.GetByKeyBatch(selectedKeys)
-	if err != nil {
-		return nil, fmt.Errorf(
-			"unable to find tasks in cache. "+
-				"Consider to run 'sneak list --refresh' to force a refresh.: %w",
-			err,
-		)
-	}
-
-	return cacheItems, nil
-}
-
 func processCloseCmd(app *App, cacheItems []*config.CacheItem, comment string) error {
 
-	if err := CloseCacheItems(app, cacheItems, "close"); err != nil {
+	if err := CloseCacheItems(app, cacheItems); err != nil {
 		return err
 	}
 
-	comment = strings.TrimSpace(comment)
-	if comment != "" {
-		_ = app.Client.AddCommentToWorkItems(
-			app.Ctx, app.LocalContext, cacheItems, comment,
-		)
-	}
+	CommentCacheItems(app, cacheItems, comment)
 
 	app.State.RemoveActiveTasks(cacheItems)
 	if err := config.SaveState(app.Dir, app.State); err != nil {
